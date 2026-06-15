574f6a364010870ab97792de8d62731579c81ea8
MikeSMR-admin <91803343+MikeSMR-admin@users.noreply.github.com>
2026-06-15 16:12:27 +0400

    v1-0-2

--- authentication--cloud-ru--for-api/config--env.sh
+++ authentication--cloud-ru--for-api/config--env.sh
@@ -1,5 +1,5 @@
 
-cd 
+cd MikeSMR-admin/ai-first--k8s--otus/authentication--cloud-ru--for-api/
 chmod +x get-token--script create-sa--script delete-sa--script menu--script
 ./menu--script.sh
 
@@ -7,7 +7,7 @@ chmod +x get-token--script create-sa--script delete-sa--script menu--script
 # Конфигурация для работы с API Cloud.ru
 # Персональный ключ доступа (или ключ от сервисного аккаунта с правами)
 export KEY_ID="9526930d266bb01011a3684cd3584f16"
-export SECRET="2d2c539d5567aa7c894f86aa0b7aa0f8"
+export SECRET=""
 
 # ID проекта (обязателен для создания сервисного аккаунта)
 export PROJECT_ID="120acc13-f4e2-471d-a896-7ba624a70b3d"
@@ -17,4 +17,4 @@ export SA_NAME="sa-managed-k8s-otus"
 export SA_DESCRIPTION="Для управления кластером Kubernetes в рамках курса OTUS"
 
 # ID сервисного аккаунта для удаления (если нужно удалить конкретный)
-export SA_ID_TO_DELETE="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxxx"
+export SA_ID_TO_DELETE=""
